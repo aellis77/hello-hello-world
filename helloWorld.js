0d78285bd52bfaa6fab671aef87e7c905c8ab649
aellis77 <aellis01@outlook.com>
2018-07-07 18:58:20 -0400

    Version 1.0

--- helloWorld.js
+++ helloWorld.js
@@ -14,7 +14,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program in the extendedLicenceInfo.txt file in this
+    along with this program in this
     directory. If not, see <https://www.gnu.org/licenses/>.
 */
 //include Yue ? Might be able to do a window in node
