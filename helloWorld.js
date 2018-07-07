5a0da5b51578ce400e7a175433ca0d4298245d7f
aellis77 <aellis01@outlook.com>
2018-07-07 18:38:51 -0400

    Version 1.0

---
+++ helloWorld.js
@@ -0,0 +1,35 @@
+ /* Program : helloWorld.js. Version 1.0 7/7/2018 
+    For some reason this is considered the first program 
+    of choice.
+    Copyright (C) 2018  Amanda Ellis, wool_neu@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program in the extendedLicenceInfo.txt file in this
+    directory. If not, see <https://www.gnu.org/licenses/>.
+*/
+//include Yue ? Might be able to do a window in node
+// https://libyue.com/docs/latest/js/guides/getting_started.html
+// well, not in production, and more complex than I want to deal with
+//
+// Start with the console.log
+console.log("Hello World!");
+console.log("-------------------------")
+console.log(" |   |   _   |   |   _   ");
+console.log(" |---|  |_|  |   |  | |  ");
+console.log(" |   |  |_   |   |  |_|  ");
+console.log("-------------------------");
+
+// then move to the message box  (not an option in node - crashes the system)
+alert( "World ? \n Where are you world ? \n O, there you are ... \n \n Well, Hello World!");
+ 
+
